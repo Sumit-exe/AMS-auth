2e82b884f30470d8a2fb7695b1f9dfcae8c91ebd
Sumit sharma <sumitsharma@ibm.com>
2024-05-23 16:35:33 +0530

    added search filter in all employees page

--- ams-frontend/src/pages/AllEmployees.js
+++ ams-frontend/src/pages/AllEmployees.js
@@ -6,11 +6,11 @@ import { GrView } from "react-icons/gr";
 import { IoClose } from "react-icons/io5";
 import WorkCharts from '../components/WorkCharts.js';
 
-
 const AllEmployees = () => {
     const [allEmployees, setAllEmployees] = useState([]);
     const [viewToggleId, setViewToggleId] = useState(null);
     const [hamActive, setHamActive] = useState(true);
+    const [searchQuery, setSearchQuery] = useState("");
 
     const handleGetAllEmployees = async () => {
         try {
@@ -26,9 +26,18 @@ const AllEmployees = () => {
         setViewToggleId(prevId => (prevId === employeeId ? null : employeeId));
     }
 
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    }
+
     useEffect(() => {
         handleGetAllEmployees();
     }, []);
+
+    const filteredEmployees = allEmployees.filter(emp => 
+        emp.employeeId.toString().includes(searchQuery) || 
+        emp.employeeFullName.toLowerCase().includes(searchQuery.toLowerCase())
+    );
 
     return (
         <div className='w-full flex justify-between max-sm:flex-col' >
@@ -38,11 +47,19 @@ const AllEmployees = () => {
             <div className={` ${hamActive ? 'w-[75%] max-sm:w-full' : 'w-full'} flex vh-100`}>
                 <div className='w-full flex flex-col items-center p-3'>
                     <div className='border-2 p-1 rounded-full w-full flex justify-between m-3'>
-                        <input type="text" placeholder='search userName' className='flex flex-1 border-0 outline-none ml-3' />
-                        <button className='bg-main-green py-2 px-4 rounded-full text-white hover:bg-green-500' onClick={handleGetAllEmployees}>search</button>
+                        <input 
+                            type="text" 
+                            placeholder='Search by Name or ID' 
+                            className='flex flex-1 border-0 outline-none ml-3' 
+                            value={searchQuery}
+                            onChange={handleSearchChange}
+                        />
+                        <button 
+                            className='bg-main-green py-2 px-4 rounded-full text-white hover:bg-green-500'
+                        >Search</button>
                     </div>
                     <div className='bg-gray-300 w-full h-[85vh] overflow-y-auto overflow-x-hidden rounded p-2'>
-                        {allEmployees?.map((emp) => (
+                        {filteredEmployees?.map((emp) => (
                             <div key={emp.employeeId} className='bg-white p-2 rounded m-1 flex flex-col justify-between w-full items-between shadow'>
                                 <div className='flex justify-between items-center '>
                                     <div className='flex gap-52 max-md:gap-6 max-sm:flex-col max-sm:gap-2'>
